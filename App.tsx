d36f96e47f3d5d96ac22cd2f19671ac32240fbcc
thou-vow <thou.vow.etoile@gmail.com>
2026-05-29 13:52:42 -0300

    feat(p2): implementa busca por nome

--- App.tsx
+++ App.tsx
@@ -1,20 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  FlatList,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+
+interface Pais {
+  name: {
+    common: string;
+    official: string;
+  };
+  translations?: {
+    rus?: {
+      common?: string;
+      official?: string;
+    };
+  };
+  flags: {
+    png: string;
+  };
+  maps: {
+    openStreetMaps: string;
+  };
+}
 
 export default function App() {
+  const [busca, setBusca] = useState('')
+  const [paises, setPaises] = useState<Pais[]>([])
+
+  const buscarPorNome = async () => {
+    try {
+      const resposta = await fetch(`https://restcountries.com/v3.1/name/${busca}`)
+
+      const primeiro: Pais = (await resposta.json())[0];
+      setPaises([primeiro]);
+    } catch (error) {
+      alert(`País não encontrado`);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder='Digite aqui o nome do país...'
+        value={busca}
+        onChangeText={(newText) => setBusca(newText)}
+      />
+      <Pressable
+        onPress={buscarPorNome}
+        style={styles.button}>
+        <Text
+          style={styles.buttonText}>
+          Buscar
+        </Text>
+      </Pressable>
+
+      <FlatList
+        style={styles.list}
+        data={paises}
+        renderItem={({ item }) => (
+          <View style={styles.listEntry}>
+            <Text style={styles.listEntryText}>
+              Nome comum: {item.name.common}
+            </Text>
+            <Text style={styles.listEntryText}>
+              Nome oficial: {item.name.official}
+            </Text>
+            <Text style={styles.listEntryText}>
+              Nome em russo: {
+                item.translations?.rus?.official
+                || item.translations?.rus?.common
+                || 'Indisponível'
+              }
+            </Text>
+            <Text style={styles.listEntryText}>
+              OpenStreetMap: {item.maps.openStreetMaps}
+            </Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+
+  },
+  buttonText: {
+
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input: {},
+  list: {},
+  listEntry: {
+
+  },
+  listEntryText: {
+
   },
 });
